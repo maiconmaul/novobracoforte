3b855ce52057b2be9c622fa6d144a3310b40f4e9
Maicon Santos <maicon.msantos@picpay.com>
2022-10-02 16:17:17 -0300

    adicionado variaveis estaticas

--- Assets/Scripts/PlanetSceneScript.cs
+++ Assets/Scripts/PlanetSceneScript.cs
@@ -5,14 +5,25 @@ using TMPro;
 
 public class PlanetSceneScript : MonoBehaviour
 {
-    public static int planetID = 1;
+    public SpriteRenderer spriteEarth;
+    public SpriteRenderer spriteMars;
     [SerializeField] private TextMeshProUGUI daysOnThisPlanet;
     int days;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(Storages.CurrentPlanetID.ToString());
+        if (Storages.CurrentPlanetID != 1)
+        {
+            this.spriteEarth.enabled = false;
+            this.spriteMars.enabled = true;
+        } 
+        else
+        {
+            this.spriteEarth.enabled = true;
+            this.spriteMars.enabled = false;
+        }
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/Storages.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Storages : MonoBehaviour
+{
+    public static SpaceShip MainSpaceShip;
+    public static int CurrentPlanetID = 1;
+
+}
