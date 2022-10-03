620993f194385698e448dfb196626f7300f36e03
jeacar1323 <jeancar1323@gmail.com>
2022-10-02 23:26:25 -0300

    adicionado fim da fase

--- Assets/Scripts/FaseScript.cs
+++ Assets/Scripts/FaseScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class FaseScript : MonoBehaviour
@@ -11,14 +12,22 @@ public class FaseScript : MonoBehaviour
     void Start()
     {
         
-        faseSlider.maxValue -= segInFas;
+        faseSlider.maxValue = segInFas;
     }
 
     // Update is called once per frame
     void Update()
     {
-        segInFas -= Time.deltaTime;
+       
         faseSlider.value += Time.deltaTime;
+        if (faseSlider.value >= (segInFas  - 1))
+        {
+            Storages.stop = true;
+            Storages.CurrentPlanetID += 1;
+            Storages.DaysInCurrentPlanet = 1;
+            SceneManager.LoadScene("PlanetScene");
+        }
+
        
 
     }
